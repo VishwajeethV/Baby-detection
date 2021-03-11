cb282decf68853e00cef6f55527422de1ba3cbcf
VishwajeethV <spacexblue80@gmail.com>
2021-03-11 18:55:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,32 +2,56 @@ humans=[];
 status="";
 
 function preload() {
-    song=loadSound("zapsplat_emergency_nuclear_power_station_meltdown_alarm_42849.mp3");   
+    song=loadSound("zapsplat_emergency_nuclear_power_station_meltdown_alarm_42849.mp3"); 
+    console.log("song is loaded");
 }
 
 function setup() {
-    canvas=createCanvas(700,500);
+    canvas=createCanvas(500,300);
     canvas.center();
-    video = createCapture(VIDEO);
+    video=createCapture(VIDEO);
     video.hide();
     objectdetector=ml5.objectDetector("cocossd",modelloaded);
-    document.getElementById("Status").innerHTML="Status - Detecting Objects";
+    document.getElementById("Status").innerHTML="status = detecting";
+
+function modelloaded() {
+    console.log("model is loaded");
+    status=true;
 }
 
+function getresults(error,results) {
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+    humans=results;
+}
+}
 
 function draw() { 
-    
+    image(video,0,0,500,300);
     if(status!="") {
+        objectdetector.detect(video,getresults);
         for(var i=0;i<humans.length;i++) {
              fill("red");
              textSize(20);
              percent=floor(humans[i].confidence*100)
-             text(humans[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+20);
+             text(humans[i].label+" "+percent+"%",humans[i].x+15,humans[i].y+20);
              noFill();
-             stroke("purple");
+             stroke("red");
              strokeWeight(5);
-             rect(humans[i].x,objects[i].y,objects[i].width,objects[i].height);
+             rect(humans[i].x,humans[i].y,humans[i].width,humans[i].height);
+             if(humans[i].label=="person"){
+               document.getElementById("Status").innerHTML="Status - Baby found";
+               song.stop();
+             }
+             else{
+                document.getElementById("Status").innerHTML="Status - Baby Missing";
+                song.play();
+             }
           }
         
     }
+  }
 }
